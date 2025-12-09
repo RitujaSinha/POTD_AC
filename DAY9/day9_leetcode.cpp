8094e01f51cfdc5b228b997208ab213dd0e3ffc9
Rituja Sinha <ritujasinha2@gmail.com>
2025-12-09 21:44:36 +0530

    solutions added

--- DAY9/day9_leetcode.cpp
+++ DAY9/day9_leetcode.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN, currSum = 0;
+    void reverse(vector<int> &nums, int st, int end){
+        // st = 0, end = nums.size()-1;
 
-        for(int i = 0; i < nums.size(); i++){
-            currSum += nums[i];
-
-            maxSum = max(maxSum, currSum);
-
-            if(currSum < 0){
-                currSum = 0;
-            }
+        while(st <= end){
+            swap(nums[st], nums[end]);
+            st++;
+            end--;
         }
-        return maxSum;
     }
-    
+
+   
+
+    void rotate(vector<int>& nums, int k) {
+
+        int n = nums.size();
+
+        k = k % n;
+
+        reverse(nums, 0 , n-k-1);
+        reverse(nums, n-k , n-1);
+        reverse(nums, 0 , n-1);
+
+    }
 };
+
+
