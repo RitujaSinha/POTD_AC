071b684c9507c8dbbbbb4becac5471d4f2ea27fc
Rituja Sinha <ritujasinha2@gmail.com>
2026-01-08 21:50:43 +0530

    DAY37 POTD Solutions

---
+++ DAY37/RaisingBacteria.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+
+    int answer = __builtin_popcount(x);
+    cout << answer << endl;
+    
+    return 0;
+}
